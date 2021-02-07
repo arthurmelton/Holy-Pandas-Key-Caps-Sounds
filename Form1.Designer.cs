190cd7483879eb5aebef3b337aa2dd0c0a8a2bd1
AMTitan <amtitan@icloud.com>
2021-02-07 10:11:53 -0500

    play sounds

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,15 +23,23 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,27 @@ namespace Holy_Pandas_Key_Caps_Sounds
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true; 
+                var sound = new SoundPlayer(@"Holy Pandas down.wav");
+                sound.Play(); 
+            }).Start();
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true; 
+                var sound = new SoundPlayer(@"Holy Pandas up.wav");
+                sound.Play(); 
+            }).Start();
         }
 
     }
